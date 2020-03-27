a3408048e0eca6de4354548b8704bdc33f230545
Bojurie Rogers-Wright <40696649+Bojurie@users.noreply.github.com>
2020-03-27 16:35:56 -0700

    Complete Project

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-// Listen for Submit
-document.getElementById('loan-form').addEventListener('submit', calculteResults);
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
 
+  setTimeout(calculateResults, 2000);
 
-// Calculte Result
+  e.preventDefault();
+});
 
-function calculteResults(e){
+// Calculate Results
+function calculateResults(){
   console.log('Calculating...');
-
   // UI Vars
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -15,50 +22,59 @@ function calculteResults(e){
   const totalPayment = document.getElementById('total-payment');
   const totalInterest = document.getElementById('total-interest');
 
-  const principle = parseFloat(amount.value);
+  const principal = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
 
-  // Compute Monthly Payment
-  const x =Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principle*x*calculatedInterest)/(x-1);
-  
-  if(isFinite(monthly)){
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments)-principle).toFixed(2);
-  }else {
-    showError('Please Check Your Numbers')  
+    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
 
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+  } else {
+    showError('Please check your numbers');
+  }
 }
 
-  e.preventDefault();
-}
-
-// Show error
+// Show Error
 function showError(error){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
 
   // Create a div
   const errorDiv = document.createElement('div');
 
-  // Get Element
+  // Get elements
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
 
-  // Add Class
+  // Add class
   errorDiv.className = 'alert alert-danger';
 
-  // Create text node and append on div
+  // Create text node and append to div
   errorDiv.appendChild(document.createTextNode(error));
 
-  // insert error above heading
+  // Insert error above heading
   card.insertBefore(errorDiv, heading);
 
   // Clear error after 3 seconds
   setTimeout(clearError, 3000);
 }
 
-// Clear error function
+// Clear error
 function clearError(){
   document.querySelector('.alert').remove();
 }
